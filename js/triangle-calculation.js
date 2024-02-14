1ac002fe5e2000d3661949e238a8af3eb18c0f39
shorot-chicham <mr.s.chicham24@gmail.com>
2024-02-14 15:37:49 +0600

    added triangle container and also fixed all functional problem and added rectangle file for the site

---
+++ js/triangle-calculation.js
@@ -0,0 +1,22 @@
+function calculateTriangleArea (){
+    const triangleBaseInput = document.getElementById('triangle-base');
+    // get triangle base value 
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+    // console.log(typeof base);
+
+    // get triangle height value
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText);
+    // console.log(height);
+
+    const area = 0.5 * base * height;
+    console.log(area);
+
+    // display triangle area 
+
+    const triangleAreaSpan = document.getElementById('triangle-area');
+    triangleAreaSpan.innerText = area;
+
+}
